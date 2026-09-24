a8aa8f209489ee3f5a1877caff747788946167ca
Vladislav Ivanov <hi.vi.777@yandex.ru>
2026-09-24 13:53:11 +0300

    VI_Task003

--- VI_Task003.py
+++ VI_Task003.py
@@ -1,1 +1,22 @@
-print("Hello!")
+surname_commission = input("Введите вашу фамилию: ")
+name_commission = input("Введите ваше имя: ")
+print(f'Добро пожаловать, {name_commission}! Начинаем приём заявок')
+
+cnt_all = 0
+cnt_approved = 0
+while True:
+    surname = input('Фамилия абитуриента (или "завершить", чтобы закончить приём): ')
+    if surname == 'завершить':
+        break
+    score = int(input('Балл абитуриента: '))
+    is_olympic = input('Есть ли диплом олимпиады? (да/нет): ').lower()
+    if (score >= 220) or (is_olympic == 'да' and score >= 180):
+        cnt_approved += 1
+        print(f'{surname}: заявка одобрена')
+    else:
+        print(f'{surname}: заявка отклонена')
+    cnt_all += 1
+
+print(f'Рассмотрено абитуриентов: {cnt_all}')
+print(f'Зачислено: {cnt_approved}')
+print(f'До свидания, {surname_commission}!')
